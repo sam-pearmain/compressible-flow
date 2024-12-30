41ca4ea472f8133896199ab9720070c587b39cb1
sam-pearmain <sam.pearmain@kcl.ac.uk>
2024-12-30 16:54:44 +0000

    added function definitions

--- src/obliqueshock.rs
+++ src/obliqueshock.rs
@@ -1,5 +1,7 @@
 #![allow(dead_code)]
 
+use std::result;
+
 use crate::isentropic::{valid_specific_heat_ratio, IsentropicFlowError}; 
 use crate::numerics::{bisection, newton_raphson};
 
@@ -36,11 +38,11 @@ pub struct ObliqueShock {
 }
 
 impl ObliqueShock {
-    pub fn from_mach_and_deflection_angle(upstream_mach: f64, deflection_angle: f64, specific_heat_ratio: f64) -> Result<ObliqueShock, IsentropicFlowError> {
+    pub fn from_mach_and_shock_angle(upstream_mach: f64, shock_angle: f64, specific_heat_ratio: f64) -> Result<ObliqueShock, IsentropicFlowError> {
         // pass
     }
-
-    pub fn from_mach_and_shock_angle(upstream_mach: f64, shock_angle: f64, specific_heat_ratio: f64) -> Result<ObliqueShock, IsentropicFlowError> {
+    
+    pub fn from_mach_and_deflection_angle(upstream_mach: f64, deflection_angle: f64, specific_heat_ratio: f64) -> Result<ObliqueShock, IsentropicFlowError> {
         // pass
     }
 
@@ -51,4 +53,47 @@ impl ObliqueShock {
 
 pub fn calculate(input: Vec<Input>, output: Output, specific_heat_ratio: f64) -> Result<f64, IsentropicFlowError> {
     // pass
+}
+
+pub fn calc_downstream_mach() -> Result<f64, IsentropicFlowError> {
+    // pass
+}
+
+pub fn calc_shock_angle() -> Result<f64, IsentropicFlowError> {
+    // pass
+}
+
+pub fn calc_deflection_angle() -> Result<f64, IsentropicFlowError> {
+    // pass
+}
+
+pub fn calc_pressure_ratio() -> Result<f64, IsentropicFlowError> {
+    // pass
+}
+
+pub fn calc_density_ratio() -> Result<f64, IsentropicFlowError> {
+    // pass
+}
+
+pub fn calc_temperature_ratio() -> Result<f64, IsentropicFlowError> {
+    // pass
+}
+
+pub fn calc_max_shock_angle() -> Result<f64, IsentropicFlowError> {
+    // calculates the maximum shock angle for a given mach number and specific heat ratio
+    // before the oblique shock detatches and becomes a normal bow shock
+}
+
+pub fn calc_normal_upstream_mach(upstream_mach: f64, shock_angle: f64) -> Result<f64, IsentropicFlowError> {
+    if upstream_mach < 1.0 {
+        return Err(IsentropicFlowError::InvalidMachNumber);
+    }
+    Ok(upstream_mach * shock_angle.sin())
+}
+
+pub fn calc_normal_downstream_mach(downstream_mach: f64, shock_angle: f64, deflection_angle: f64) -> Result<f64, IsentropicFlowError> {
+    if downstream_mach < 1.0 {
+        return Err(IsentropicFlowError::InvalidMachNumber);
+    }
+    Ok(downstream_mach * (shock_angle - deflection_angle).sin())
 }
