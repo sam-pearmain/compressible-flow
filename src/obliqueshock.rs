536bb329ffd6ff67c0b4627aaf4dd43389da6a95
sam-pearmain <sam.pearmain@kcl.ac.uk>
2024-12-30 15:46:25 +0000

    added obliqueshock skeleton

--- src/obliqueshock.rs
+++ src/obliqueshock.rs
@@ -23,15 +23,32 @@ pub enum Output {
 }
 
 pub struct ObliqueShock {
-    upstream_mach: f64,
-    downstream_mach: f64, 
-    deflection_angle: f64,
-    shock_angle: f64,
-    pressure_ratio: f64,
-    density_ratio: f64,
-    temperature_ratio: f64,
-    stagnation_pressure_ratio: f64,
-    normal_upstream_mach: f64,
-    normal_downstream_mach: f64,
+    upstream_mach: f64,             // M1
+    downstream_mach: f64,           // M2
+    deflection_angle: f64,          // θ
+    shock_angle: f64,               // β
+    pressure_ratio: f64,            // p2 / p1
+    density_ratio: f64,             // ρ2 / ρ1
+    temperature_ratio: f64,         // T2 / T1
+    stagnation_pressure_ratio: f64, // p02 / p01
+    normal_upstream_mach: f64,      // M1n
+    normal_downstream_mach: f64,    // M2n
 }
 
+impl ObliqueShock {
+    pub fn from_mach_and_deflection_angle(upstream_mach: f64, deflection_angle: f64, specific_heat_ratio: f64) -> Result<ObliqueShock, IsentropicFlowError> {
+        // pass
+    }
+
+    pub fn from_mach_and_shock_angle(upstream_mach: f64, shock_angle: f64, specific_heat_ratio: f64) -> Result<ObliqueShock, IsentropicFlowError> {
+        // pass
+    }
+
+    pub fn from_mach_and_normal_mach(upstream_mach: f64, normal_upstream_mach: f64, specific_heat_ratio: f64) -> Result<ObliqueShock, IsentropicFlowError> {
+        // pass
+    }
+}
+
+pub fn calculate(input: Vec<Input>, output: Output, specific_heat_ratio: f64) -> Result<f64, IsentropicFlowError> {
+    // pass
+}
